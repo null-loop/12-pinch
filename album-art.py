442f0198aabd4882dbd05b53c68d8fe0d5f6f3b6
danielgray <daniel.gray@puregym.com>
2024-11-25 22:47:34 +0000

    Remove file usage - attempt 2

--- album-art.py
+++ album-art.py
@@ -34,7 +34,7 @@ try:
             response = requests.get(current_image_url)
             last_image_url = current_image_url
             print("Updating image to " + current_album['name'])
-            image = Image.open(BytesIO(response.content))
+            image = Image.open(response.raw)
             image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
             matrix.SetImage(image.convert('RGB'))
         time.sleep(1)
