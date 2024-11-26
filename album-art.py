ca6b3f68e2fb3242be5f5c68f2b009fd64b2e7b2
danielgray <daniel.gray@puregym.com>
2024-11-26 00:55:03 +0000

    Anti-flicker

--- album-art.py
+++ album-art.py
@@ -11,7 +11,7 @@ options.rows = 64
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
+options.hardware_mapping = 'adafruit-hat-pwm'
 
 matrix = RGBMatrix(options = options)
 
