34775805db48b03d7e13b8d5571b1a1c6ac7f50b
Daniel Gray <daniel.gray@puregym.com>
2025-01-02 18:25:25 +0000

    strip line endings

--- github-monitor.py
+++ github-monitor.py
@@ -1,7 +1,7 @@
 import time, sys, json, os
 from github import Github
 from github import Auth
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+#from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
 class BuildStatus:
     label = ""
@@ -21,34 +21,34 @@ def update_repo(repo_name, label):
             status.branch_count = repo.get_branches().totalCount
             return status
 
-def render_status(status, row):
+#def render_status(status, row):
     # todo: calc y offset from row
-    y_offset = 2
-    graphics.DrawText(matrix, font, 2, y_offset, white, status.label)
+    #y_offset = 2
+    #graphics.DrawText(matrix, font, 2, y_offset, white, status.label)
 
-options = RGBMatrixOptions()
-options.rows = 64
-options.cols = 64
-options.chain_length = 4
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
-options.gpio_slowdown = 2
-options.limit_refresh_rate_hz = 120
-options.brightness = 50
-options.disable_hardware_pulsing = True
-options.drop_privileges = False
+#options = RGBMatrixOptions()
+#options.rows = 64
+#options.cols = 64
+#options.chain_length = 4
+#options.parallel = 1
+#options.hardware_mapping = 'adafruit-hat'
+#options.gpio_slowdown = 2
+#options.limit_refresh_rate_hz = 120
+#options.brightness = 50
+#options.disable_hardware_pulsing = True
+#options.drop_privileges = False
 
-matrix = RGBMatrix(options = options)
+#matrix = RGBMatrix(options = options)
 
-font = graphics.Font()
-font = font.LoadFont("10x20.bdf")
-white = graphics.Color(0, 0, 0)
-red = graphics.Color(255, 0, 0)
-green = graphics.Color(0, 255, 0)
-blue = graphics.Color(0, 0, 255)
+#font = graphics.Font()
+#font = font.LoadFont("10x20.bdf")
+#white = graphics.Color(0, 0, 0)
+#red = graphics.Color(255, 0, 0)
+#green = graphics.Color(0, 255, 0)
+#blue = graphics.Color(0, 0, 255)
 
 with open('.github-secret', 'r') as github_secret_file:
-    github_pat = github_secret_file.read()
+    github_pat = github_secret_file.read().rstrip('`n')
 
 auth = Auth.Token(github_pat)
 
