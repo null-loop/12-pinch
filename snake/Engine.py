b6a6b4d1a01dbe4a8d76bd8e57120e3afee4825a
Daniel Gray <daniel.gray@puregym.com>
2025-01-11 13:21:44 +0000

    State display

--- snake/Engine.py
+++ snake/Engine.py
@@ -39,7 +39,7 @@ class Engine:
 
     def turn(self):
 
-        if self.__turn % 100 == 0:
+        if self.__turn % 1000 == 0:
             self.__print_state()
         self.__turn = self.__turn + 1
 
