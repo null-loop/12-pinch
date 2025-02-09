ec0b1d365a9133e2df1eb3541133803517890225
Daniel Gray <daniel.gray@puregym.com>
2025-02-09 13:54:46 +0000

    GOL - Turns

--- game-of-life.py
+++ game-of-life.py
@@ -5,6 +5,8 @@ from life.Engine import Engine
 from life.GameOptions import GameOptions
 
 game_options = GameOptions()
+game_options.height = 64
+game_options.width = 32
 game_board = Board(game_options)
 game_engine = Engine(game_board, game_options)
 game_engine.starting_spawn()
