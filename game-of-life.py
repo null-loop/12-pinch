6029a38f62b18324954c0c82837efc9accd68aa9
Daniel Gray <daniel.gray@puregym.com>
2025-02-09 14:56:39 +0000

    3 scale!?

--- game-of-life.py
+++ game-of-life.py
@@ -5,9 +5,9 @@ from life.Engine import Engine
 from life.GameOptions import GameOptions
 
 game_options = GameOptions()
-game_options.height = 64
-game_options.width = 64
-game_options.render_scale = 2
+game_options.height = 42
+game_options.width = 42
+game_options.render_scale = 3
 game_board = Board(game_options)
 game_engine = Engine(game_board, game_options)
 game_engine.starting_spawn()
