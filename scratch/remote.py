51d2e0bbb70b92d54220f8fcf958c5cf8646818c
Daniel Gray <daniel.gray@puregym.com>
2025-02-10 17:49:14 +0000

    Remote stub

--- scratch/remote.py
+++ scratch/remote.py
@@ -1,6 +1,6 @@
 import keyboard
 
-keyboard.hook(lambda key_event:print(key_event.name))
+keyboard.hook(lambda key_event:print(f'Name:{key_event.name}, ScanCode:{key_event.scan_code}'))
 
 while True:
     x=1
