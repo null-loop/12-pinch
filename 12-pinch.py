aeeeb36f4e849387e7e9f4c3b18221f530598d6a
Daniel Gray <daniel.gray@puregym.com>
2025-02-10 18:52:57 +0000

    Duh!

--- 12-pinch.py
+++ 12-pinch.py
@@ -1,46 +1,4 @@
-import sys
-import time
-from datetime import datetime, timedelta
-
-from screens.GitScreen import GitScreen
-from screens.LifeScreen import LifeScreen
 from screens.ScreenController import ScreenController
-from screens.SnakeScreen import SnakeScreen
-from screens.SpotifyScreen import SpotifyScreen
-from utils import matrix
-
 
 controller = ScreenController()
 controller.run()
-
-try:
-    print("Press CTRL-C to stop.")
-    screens = [LifeScreen(),GitScreen(),SnakeScreen(),SpotifyScreen()]
-    current_screen_index = 0
-    current_screen = screens[0]
-    if not current_screen.render_as_image:
-        current_screen.fresh_render()
-    screen_cycle_interval_seconds = 2 * 60 # how long until we switch to next screen
-    last_screen_cycle_time = datetime.now()
-    while True:
-        if datetime.now() > last_screen_cycle_time + timedelta(0, screen_cycle_interval_seconds):
-            last_screen_cycle_time = datetime.now()
-            current_screen_index = current_screen_index + 1
-            if current_screen_index == len(screens):
-                current_screen_index = 0
-            current_screen = screens[current_screen_index]
-            if not current_screen.render_as_image:
-                current_screen.fresh_render()
-
-        if current_screen.render_as_image:
-            image = current_screen.render()
-            if not image is None:
-                matrix.render_image_on_matrix(image)
-        else:
-            current_screen.render()
-
-        if current_screen.update_interval_seconds > 0:
-            time.sleep(current_screen.update_interval_seconds)
-
-except KeyboardInterrupt:
-    sys.exit(0)
